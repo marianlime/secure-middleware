f8a991e34b545cdb2504e5b905e9fa410387fbd5
Marian Chihai <2021529@brunel.ac.uk>
2024-01-30 00:13:12 +0000

    Added key generation method to encryption package

--- packages/encryption/encryption.go
+++ packages/encryption/encryption.go
@@ -1,1 +1,18 @@
-package aes
+package crypto
+
+import(
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"io"
+)
+
+func keyGenerator() ([]byte, error){
+	key := make([]byte, 32)
+	if _,err := rand.Read(key); err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
---
+++ packages/encryption/encryption_test.go
@@ -0,0 +1,10 @@
+package crypto_test
+
+import(
+	"testing"
+	"bytes"
+)
+
+func TestEncryptionDecryption(t *testing.T){
+	
+}
