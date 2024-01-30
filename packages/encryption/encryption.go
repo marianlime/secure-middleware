441b4863fa73d22f4e4404cb5b4dbe9fe261f51a
Marian Chihai <2021529@brunel.ac.uk>
2024-01-30 18:31:25 +0000

    added enryption and decryption methods

--- packages/encryption/encryption.go
+++ packages/encryption/encryption.go
@@ -23,6 +23,38 @@ func DataEncryption(plainText []byte, key []byte) ([]byte, error){
 	}
 
 	gcm, err := cipher.NewGCM(block)
-	
+	if err != nil {
+		return nil, err
+	}
 
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	cipherText := gcm.Seal(nonce, nonce, plainText, nil)
+	return cipherText, nil
+}
+
+func DataDecryption(cipherText []byte, key []byte) ([]byte, error){
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	
+	if len(cipherText) < gcm.NonceSize(){
+		return nil, errors.New("cipherText too short")
+	}
+
+	nonce, cipherText := cipherText[:gcm.NonceSize()], cipherText[gcm.NonceSize():]
+	plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+	if err != nil{
+		return nil, err
+	}
+
+	return plainText, nil
 }
