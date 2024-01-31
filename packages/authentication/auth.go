5e072cb246005fd1d83ef783a7c4e8a05e3672ff
MarianLime <marianchihai2002@gmail.com>
2024-01-31 10:02:49 +0000

    Update auth.go

--- packages/authentication/auth.go
+++ packages/authentication/auth.go
@@ -22,3 +22,31 @@ func GenerateToken(role string) (string, error){
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte("Secret"))
 }
+func RoleMiddleware(requiredRole string) func(http.HandlerFunc) http.HandlerFunc {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+				tokenString := r.Header.Get("Authorization")
+				tokenString = strings.TrimPrefix(tokenString, "Bearer")
+			
+				
+				token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error){
+					return []byte("Secret"), nil
+				})
+
+
+				if err != nil {
+					http.Error(w, "Unauthorized: "+err.Error(), http.StatusUnauthorized)
+					return
+				}
+
+				if claims, ok := token.Claims.(*CustomClaims); ok && token.valid{
+					if claims.Role == requiredRole {
+						next.ServeHTTP(w, r)
+						return
+					}
+				}
+
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+	}
+}
